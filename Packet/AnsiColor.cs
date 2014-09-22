e7c6aac7d7a3d786a79a5f867e75a75754aef292
Kevin <kevinr@firstpac.com>
2014-09-22 16:30:05 -0700

    more

--- Packet/AnsiColor.cs
+++ Packet/AnsiColor.cs
@@ -26,52 +26,51 @@ namespace Utility.AnsiColor
         static AnsiColor ( )
         {
             // Our reset values turns everything to the default mode
-            colorTable.Add(new ColorData("{reset}", "\x1B[0m", "Reset"));
+            colorTable.Add(new ColorData("{reset}", "\x1B[0m")); // "Reset"
 
+            /*
             // Style Modifiers (on)
-            colorTable.Add ( new ColorData ( "{bold}",      "\x1B[1m", "Bold" ) );
-            colorTable.Add ( new ColorData ( "{italic}",    "\x1B[3m", "Italic" ) );
-            colorTable.Add ( new ColorData ( "{ul}",        "\x1B[4m", "Underline" ) );
-            colorTable.Add ( new ColorData ( "{blink}",     "\x1B[5m", "Blink" ) );
-            colorTable.Add ( new ColorData ( "{blinkf}",    "\x1B[6m", "Blink Fast" ) );
-            colorTable.Add ( new ColorData ( "{inverse}",   "\x1B[7m", "Inverse" ) );
-            colorTable.Add ( new ColorData ( "{strike}",    "\x1B[9m", "Strikethrough" ) );
+            colorTable.Add(new ColorData("{bold}", "\x1B[1m")); // "Bold" 
+            colorTable.Add(new ColorData ( "{italic}", "\x1B[3m")); // "Italic" 
+            colorTable.Add(new ColorData("{ul}", "\x1B[4m")); // "Underline" 
+            colorTable.Add(new ColorData("{blink}", "\x1B[5m")); // "Blink" 
+            colorTable.Add(new ColorData("{blinkf}", "\x1B[6m")); // "Blink Fast" 
+            colorTable.Add(new ColorData("{inverse}", "\x1B[7m")); // "Inverse" 
+            colorTable.Add(new ColorData("{strike}", "\x1B[9m")); // "Strikethrough" 
 
             // Style Modifiers (off)
-            colorTable.Add ( new ColorData ( "{!bold}",     "\x1B[22m", "Bold Off" ) );
-            colorTable.Add ( new ColorData ( "{!italic}",   "\x1B[23m", "Italic Off" ) );
-            colorTable.Add ( new ColorData ( "{!ul}",       "\x1B[24m", "Underline Off" ) );
-            colorTable.Add ( new ColorData ( "{!blink}",    "\x1B[25m", "Blink Off" ) );
-            colorTable.Add ( new ColorData ( "{!inverse}",  "\x1B[27m", "Inverse Off" ) );
-            colorTable.Add ( new ColorData ( "{!strike}",   "\x1B[29m", "Strikethrough Off" ) );
-            
+            colorTable.Add(new ColorData("{!bold}", "\x1B[22m")); // "Bold Off" 
+            colorTable.Add(new ColorData("{!italic}", "\x1B[23m")); // "Italic Off" 
+            colorTable.Add(new ColorData("{!ul}", "\x1B[24m")); // "Underline Off" 
+            colorTable.Add(new ColorData("{!blink}", "\x1B[25m")); // "Blink Off" 
+            colorTable.Add(new ColorData("{!inverse}", "\x1B[27m")); // "Inverse Off" 
+            colorTable.Add(new ColorData("{!strike}", "\x1B[29m")); // "Strikethrough Off" 
+            */
             // Foreground Color
-            colorTable.Add ( new ColorData ( "{black}",     "\x1B[01;30m", "Foreground black" ) );
-            colorTable.Add ( new ColorData ( "{red}",       "\x1B[01;31m", "Foreground red" ) );
-            colorTable.Add ( new ColorData ( "{green}",     "\x1B[01;32m", "Foreground green" ) );
-            colorTable.Add ( new ColorData ( "{yellow}",    "\x1B[01;33m", "Foreground yellow" ) );
-            colorTable.Add ( new ColorData ( "{blue}",      "\x1B[01;34m", "Foreground blue" ) );
-            colorTable.Add ( new ColorData ( "{magenta}",   "\x1B[01;35m", "Foreground magenta" ) );
-            colorTable.Add ( new ColorData ( "{cyan}",      "\x1B[01;36m", "Foreground cyan" ) );
-            colorTable.Add ( new ColorData ( "{white}",     "\x1B;01;37m", "Foreground white" ) );
+            colorTable.Add(new ColorData("{black}", "\x1B[01;30m")); // "Foreground black" 
+            colorTable.Add(new ColorData("{red}", "\x1B[01;31m")); // "Foreground red" 
+            colorTable.Add(new ColorData("{green}", "\x1B[01;32m")); // "Foreground green" 
+            colorTable.Add(new ColorData("{yellow}", "\x1B[01;33m")); // "Foreground yellow" 
+            colorTable.Add(new ColorData("{blue}", "\x1B[01;34m")); // "Foreground blue" 
+            colorTable.Add(new ColorData("{magenta}", "\x1B[01;35m")); // "Foreground magenta" 
+            colorTable.Add(new ColorData("{cyan}", "\x1B[01;36m")); // "Foreground cyan" 
+            colorTable.Add(new ColorData("{white}", "\x1B;01;37m")); // "Foreground white" 
 
             // Background Color
-            colorTable.Add ( new ColorData ( "{!black}",    "\x1B[40m", "Background black" ) );
-            colorTable.Add ( new ColorData ( "{!red}",      "\x1B[41m", "Background red" ) );
-            colorTable.Add ( new ColorData ( "{!green}",    "\x1B[42m", "Background green" ) );
-            colorTable.Add ( new ColorData ( "{!yellow}",   "\x1B[43m", "Background yellow" ) );
-            colorTable.Add ( new ColorData ( "{!blue}",     "\x1B[44m", "Background blue" ) );
-            colorTable.Add ( new ColorData ( "{!magenta}",  "\x1B[45m", "Background magenta" ) );
-            colorTable.Add ( new ColorData ( "{!cyan}",     "\x1B[46m", "Background cyan" ) );
-            colorTable.Add ( new ColorData ( "{!white}",    "\x1B[47m", "Background white" ) );
+            colorTable.Add(new ColorData("{!black}", "\x1B[40m")); // "Background black" 
+            colorTable.Add(new ColorData("{!red}", "\x1B[41m")); // "Background red" 
+            colorTable.Add(new ColorData("{!green}", "\x1B[42m")); // "Background green" 
+            colorTable.Add(new ColorData("{!yellow}", "\x1B[43m")); // "Background yellow" 
+            colorTable.Add(new ColorData("{!blue}", "\x1B[44m")); // "Background blue" 
+            colorTable.Add(new ColorData("{!magenta}", "\x1B[45m")); // "Background magenta" 
+            colorTable.Add(new ColorData("{!cyan}", "\x1B[46m")); // "Background cyan" 
+            colorTable.Add(new ColorData("{!white}", "\x1B[47m")); // "Background white" 
         } // End of AnsiColor
 
         #endregion
 
         //---------------------------------------------------------------------------------------------------------
         // static string Colorize
-        // The colorize string will take in a string that contains metadata for ansi colors. It will
-        // then replace all of the metadata with the actual color identifiers.
         //---------------------------------------------------------------------------------------------------------
         #region static string Colorize
         public  string Colorize ( string stringToColor )
@@ -94,7 +93,7 @@ namespace Utility.AnsiColor
         #region Private Variables
         string identifier;
         string code;
-        string definition;
+        //string definition;
 
         #endregion
 
@@ -112,22 +111,12 @@ namespace Utility.AnsiColor
         } // End of ReadOnly Identifier
         #endregion
 
-        #region public string Definition
-        public string Definition
-        {
-            get { return definition; }
-        } // End of ReadOnly Definition
-
-        #endregion
-
-
         #region public ColorData
-        public ColorData ( string identifier, string code, string definition )
+        public ColorData(string identifier, string code) 
         {
             // Set our values
             this.identifier = identifier;
             this.code = code;
-            this.definition = definition;
         } // End of ColorData Constructor
         #endregion
     } // End of ColorData structure
