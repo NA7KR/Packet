043d52ab1eb0c78aaa1555ba4597800838b17042
Kevin <kevin@na7kr.us>
2014-09-28 21:09:47 -0700

    more

--- Packet/ModifyFile.cs
+++ Packet/ModifyFile.cs
@@ -22,7 +22,6 @@ namespace Utility.ModifyFile
                 {
                     // Try to create the directory.
                     DirectoryInfo di = Directory.CreateDirectory(path);
-                    //Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
                 }
 
                 path = path + @"\myMailList.txt";
