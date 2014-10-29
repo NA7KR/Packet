495c7a5e710f34df91819e9e717b1f7fdde57b98
Kevin <kevin@na7kr.us>
2014-10-29 10:12:56 -0700

    8

--- TerminalControl/StringResource.cs
+++ TerminalControl/StringResource.cs
@@ -4,9 +4,7 @@ using System.Resources;
 
 namespace PacketComs
 {
-    /// <summary>
-    /// StringResource �̊T�v�̐���ł��B
-    /// </summary>
+    
     internal class StringResources
     {
         private string _resourceName;
@@ -22,7 +20,7 @@ namespace PacketComs
         {
             try
             {
-                return _resMan.GetString(id); //������ꂪ�x���悤�Ȃ炱�̃N���X�ŃL���b�V���ł����΂������낤
+                return _resMan.GetString(id); 
             }
             catch
             {
@@ -32,11 +30,7 @@ namespace PacketComs
 
         private void LoadResourceManager(string name, Assembly asm)
         {
-            //���ʂ͉p��E���{�ꂵ�����Ȃ�
             CultureInfo ci = System.Threading.Thread.CurrentThread.CurrentUICulture;
-            //if(ci.Name.StartsWith("ja"))
-            //_resMan = new ResourceManager(name+"_ja", asm);
-            //else
             _resMan = new ResourceManager(name, asm);
         }
     }
