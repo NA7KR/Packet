f9f4208ce08ac57f2856c6810bc5d3851f486992
Kevin <kevinr@firstpac.com>
2014-10-31 16:42:43 -0700

    New SSH

--- Packet/Form1.cs
+++ Packet/Form1.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Windows.Forms;
 using PacketComs;
 
+
 #endregion
 
 namespace Packet
--- TerminalControl/TerminalControl.cs
+++ TerminalControl/TerminalControl.cs
@@ -13,6 +13,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
+using Renci.SshNet;
 
 #endregion
 
