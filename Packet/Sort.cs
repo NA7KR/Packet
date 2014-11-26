662c3f635e79abc792fd4e57323c12eb0b95e0ad
Kevin <kevin@na7kr.us>
2014-11-25 22:57:08 -0800

    save

--- Packet/Sort.cs
+++ Packet/Sort.cs
@@ -22,19 +22,36 @@ namespace Packet
        
         private void button_ok_Click(object sender, EventArgs e)
         {
+            //string myString = listView1.SelectedItems;
 
-            _myFiles.WriteST(listView1.SelectedItems.ToString(), "SelectedTo"); 
+            string myString = string.Empty;
+            foreach (ListViewItem.Selected anItem in listView1.Items)
+            {
+                 myString += "," + anItem.Text;
+            }
+
+
+            _myFiles.WriteST(myString, "SelectedTo"); 
         }
 
         private void Sort_Load(object sender, EventArgs e)
         {
             listView1.Left =  5;
-            listView1.Width = (Width - 50);
+            listView1.Width = (Width - 30);
             listView1.Top = 5;
             listView1.Height = (Height - 100);
             button_ok.Top = (Height - 75);
             button_Cancel.Top = (Height - 75);
 
+            /*
+             int selectedItem = Properties.Settings.Default.SelectedItem;
+                if (selectedItem != -1)
+                {
+                   this.listView1.Items[selectedItem].Selected = true;
+                }
+             */
+
+            
             string myString = _myFiles.RXST("SortTo");
             string[] myArray = myString.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             foreach (string s in myArray)
@@ -52,7 +69,7 @@ namespace Packet
         private void Sort_Resize(object sender, EventArgs e)
         {
             listView1.Left = 5;
-            listView1.Width = (Width - 50);
+            listView1.Width = (Width - 30);
             listView1.Top = 5;
             listView1.Height = (Height - 100);
             button_ok.Top = (Height - 75);
