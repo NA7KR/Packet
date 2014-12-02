20da450c84c07806948a2eb057b2cdb6f9705d17
Kevin <kevinr@firstpac.com>
2014-12-02 14:18:30 -0800

    sql

--- TerminalControl/ModifyFile.cs
+++ TerminalControl/ModifyFile.cs
@@ -2,198 +2,216 @@
 
 using System;
 using System.Data.Odbc;
-using System.Data.SqlTypes;
 using System.IO;
-using System.Resources;
 using System.Windows.Forms;
-
 
 #endregion
 
 namespace PacketComs
-{
-    public class ModifyFile
     {
-        OdbcConnection myConnection = new OdbcConnection("DSN=Packet");
-        public  ModifyFile()
-        {  
-            myConnection.Open();
-            if (SQLMakeTable() )
-            { }
-        }
-
-        public string SQL(string SQLCommand)
+    public class ModifyFile
         {
+        OdbcConnection myConnection = new OdbcConnection("DSN=Packet");
+        public ModifyFile()
+            {
+            myConnection.Open();
+            if
+            (SQLInsert("CREATE TABLE  Packet ( MSG int PRIMARY KEY, MSGTSLD CHAR(3), MSGSize int, MSGTO CHAR(6), MSGRoute CHAR(7),MSGFrom CHAR(6), MSGDateTime CHAR(9), MSGSubject CHAR(30), MSGState CHAR(8)     )"))
+            { }
+            }
+
+        #region SQL
+        public string SQL(string SQLCommand)
+            {
             OdbcCommand myCommand = myConnection.CreateCommand();
             myCommand.CommandText = SQLCommand;
             OdbcDataReader myReader = myCommand.ExecuteReader();
-            
-         
             return myReader.ToString();
-        }
-
-        public bool SQLMakeTable()
-            {
-            OdbcCommand myCommand = myConnection.CreateCommand();
-            myCommand.CommandText  =  "CREATE TABLE  Packet ( MSG int PRIMARY KEY, MSGTSLD CHAR(3), MSGSize int, MSGTO CHAR(6), MSGRoute CHAR(7), MSGDateTime CHAR(9), MSGSubject CHAR(30), MSGState CHAR(8)     )";
-
-
-            myCommand.ExecuteNonQuery();
-            return true;
             }
+        #endregion
 
-        #region Write
 
-        public bool Write(string textVale)
+        #region SQLInsert
+        public bool SQLInsert(string stTable)
         {
             try
-            {
-                string path = Directory.GetCurrentDirectory() + @"\Data";
-                if (!Directory.Exists(path))
                 {
-                    // Try to create the directory.
-                    Directory.CreateDirectory(path);
+                   OdbcCommand myCommand = myConnection.CreateCommand();
+                    myCommand.CommandText = stTable;
+                    myCommand.ExecuteNonQuery();
+                    return true;
                 }
-                path = path + @"\myMailList.txt";
-                File.AppendAllText(path, textVale);
-                return true;
-            } //end try
-            catch (Exception e)
+            catch (OdbcException e)
             {
                 MessageBox.Show(e.Message);
                 return false;
             }
-        } //end write
+       }
+
+        #endregion
+
+        #region Write
+
+        public bool Write(string textVale)
+            {
+            try
+                {
+                SQLInsert("INSERT INTO  Packet " +
+                             "( MSG," +
+                             "MSGTSLD," +
+                             "MSGSize," +
+                             "MSGTO," +
+                             "MSGRoute," +
+                             "MSGFrom," +
+                             "MSGDateTime," +
+                             "MSGSubject," +
+                             "MSGState  ) VALUES ( '101','BF','1504','SWPC','@WW','CX2SA','0318/1811','Solar Region Summary','')");
+                string path = Directory.GetCurrentDirectory() + @"\Data";
+                if (!Directory.Exists(path))
+                    {
+                    // Try to create the directory.
+                    Directory.CreateDirectory(path);
+                    }
+                path = path + @"\myMailList.txt";
+                File.AppendAllText(path, textVale);
+                return true;
+                } //end try
+            catch (Exception e)
+                {
+                MessageBox.Show(e.Message);
+                return false;
+                }
+            } //end write
 
         #endregion
 
         #region WriteST
 
-        public bool WriteST(string textVale,string fileName)
-        {
-            
-            try
+        public bool WriteST(string textVale, string fileName)
             {
+
+            try
+                {
                 string path = Directory.GetCurrentDirectory() + @"\Data";
                 if (!Directory.Exists(path))
-                {
+                    {
                     // Try to create the directory.
                     Directory.CreateDirectory(path);
-                }
+                    }
                 string filePath = path + @"\" + fileName + ".txt";
 
                 File.WriteAllText(filePath, textVale);
                 return true;
-            } //end try
+                } //end try
             catch (Exception e)
-            {
+                {
                 MessageBox.Show(e.Message);
                 return false;
-            }
-        } //end write
+                }
+            } //end write
 
         #endregion
 
         #region DeleteST
         public bool? DeleteST(string fileName)
-        {
+            {
 
             try
-            {
+                {
                 string path = Directory.GetCurrentDirectory() + @"\Data";
                 if (!Directory.Exists(path))
-                {
+                    {
                     // Try to create the directory.
                     Directory.CreateDirectory(path);
-                }
+                    }
                 path = path + @"\" + fileName + ".txt";
                 if (File.Exists(path))
-                {
+                    {
                     File.Delete(path);
                     return true;
-                }
-            
+                    }
+
                 return null;
-                
-                
-            } //end try
+
+
+                } //end try
             catch (Exception e)
-            {
+                {
                 MessageBox.Show(e.Message);
                 return false;
-            }
-        } //end delete
+                }
+            } //end delete
         #endregion
 
         #region checkST
         public bool? CheckST(string fileName)
-        {
+            {
 
             try
-            {
+                {
                 string path = Directory.GetCurrentDirectory() + @"\Data";
                 if (!Directory.Exists(path))
-                {
+                    {
                     // Try to create the directory.
                     Directory.CreateDirectory(path);
-                }
+                    }
                 path = path + @"\" + fileName + ".txt";
                 if (File.Exists(path))
-                {
+                    {
                     return true;
-                }
+                    }
                 return null;
-            } //end try
+                } //end try
             catch (Exception e)
-            {
+                {
                 MessageBox.Show(e.Message);
                 return false;
-            }
-        } //end check
+                }
+            } //end check
         #endregion
 
         #region RXST
         public string RXST(string fileName)
-        {
+            {
             string myString = null;
             string path = Directory.GetCurrentDirectory() + @"\Data";
             if (!Directory.Exists(path))
-            {
+                {
                 // Try to create the directory.
                 Directory.CreateDirectory(path);
-            }
+                }
             path = path + @"\" + fileName + ".txt";
             if (File.Exists(path))
-            {
+                {
                 StreamReader myFile = new StreamReader(path);
                 myString = myFile.ReadToEnd();
                 myFile.Close();
-            }
+                }
             return myString;
 
-        }
+            }
         #endregion
-   
+
 
         #region RX
         public string RX()
-        {
+            {
             string myString = null;
             string path = Directory.GetCurrentDirectory() + @"\Data";
             if (!Directory.Exists(path))
-            {
+                {
                 // Try to create the directory.
                 Directory.CreateDirectory(path);
-            }
+                }
             path = path + @"\myMailList.txt";
 
             if (File.Exists(path))
-            {
+                {
                 StreamReader myFile = new StreamReader(path);
-                myString = myFile.ReadToEnd();  
-            }
+                myString = myFile.ReadToEnd();
+                }
             return myString;
-        }
+            }
         #endregion
-    }
-} //end name-space
+        }
+    } //end name-space
