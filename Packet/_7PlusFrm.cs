1e1d7c55d0e6dd55c448d6918b269384875c3f9a
Kevin <kevin@na7kr.us>
2015-02-27 16:25:14 -0800

    KRR

--- Packet/_7PlusFrm.cs
+++ Packet/_7PlusFrm.cs
@@ -9,16 +9,21 @@ namespace Packet
 
 public partial class _7PlusFrm : Form
     {
-    
+    [DllImport("7plus.dll")]
+    public static extern void Do_7plus(string args);
         public _7PlusFrm()
         {
             InitializeComponent();
         }
 
-        private void button_ok_Click(object sender, System.EventArgs e)
+        private void button_ok_Click(object sender, EventArgs e)
         {
+            Do_7plus("c:\\temp\\7plus.zip -SAVE \"c:\\temp\\\"  -SB 5000");
 
-          
+            //    c:\temp\7plus.zip -SAVE "c:\temp\"  -SB 5000          
+            //    c:\temp\7plus.p01 - SAVE "c:\temp\"                 
+            //    c:\temp\7plus.err -SAVE "c:\temp\"					
+            //	  c:\temp\7plus.cor -SAVE "c:\temp\	
 
         }
     }
