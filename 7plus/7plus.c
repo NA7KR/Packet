a06326194fb3490c3bbb87be620661b990dba569
Kevin <kevin@na7kr.us>
2015-03-03 14:09:39 -0800

    Added 7plus to mail project

---
+++ 7plus/7plus.c
@@ -0,0 +1,536 @@
+/* Version */
+#define VERSION "2.25"
+#define PL7_DATE "20000320"
+
+#include "7plus.h"
+#include <conio.h>
+
+FILE* o;
+uint crctab[256];
+byte decode[256];
+byte code[216];
+char range[257];
+byte _extended = '*'; /* Allow long filenames */
+size_t buflen;
+char _drive[MAXDRIVE], _dir[MAXDIR], _file[MAXFILE], _ext[MAXEXT];
+char spaces[] = "                                                   ";
+char* endstr;
+char* sendstr;
+char* pathstr;
+char genpath[MAXPATH];
+char altname[MAXPATH];
+char delimit[] = "\n";
+char def_format[] = "format.def";
+const char cant[] = "\007\n'%s': Can't open. Break.\n";
+const char notsame[] = "\007Filesize in %s differs from the original file!\n" "Break.\n";
+const char nomem[] = "\007Argh error: Not enough memory present! " "Can't continue.....\n";
+int noquery = 0;
+int force = 0;
+int fls = 0;
+int autokill = 0;
+int simulate = 0;
+int sysop = 0;
+int no_tty = 0;
+int twolinesend = 0;
+struct m_index* idxptr;
+#define _LFN "/LFN"
+
+const char s_logon[] = "\n[7+ v"VERSION""_LFN" ("PL7_DATE"), (C) DG1BBQ]\n";
+
+HANDLE hDLLInst = 0;
+
+BOOL WINAPI DllMain(HANDLE hModule, DWORD dwFunction, LPVOID lpNot)
+{
+	hDLLInst = hModule;
+
+	switch (dwFunction)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_PROCESS_DETACH:
+	default:
+		break;
+	}
+	return TRUE;
+}
+
+
+// The real DLL entry point
+//int __export CALLBACK Do_7plus(char *cmd_line)
+__declspec(dllexport) int  Do_7plus(char *cmd_line)
+{
+	char *p1, *p2;
+	char **argv;
+	int argc = 0;
+	int i, l;
+	int ret;
+
+	/*
+		* Count the args. Long Windows 9x file names may contain spaces,
+		* a long file name could look like this... "This is a long win9x file called fumph.zip" Note the " " surrounding the file name.
+		*/
+	l = strlen(cmd_line);
+
+	for (i = 0; i <= l; i++)
+	{
+		if (cmd_line[i] == '"')
+		{
+			i++;
+			while (cmd_line[i] != '"') i++;
+			i++;
+		}
+		//	* Replace ' ' with '\0' unless surrounded with quotes to indicate the spaces are inside a long file name.
+		if (cmd_line[i] == ' ')
+		{
+			cmd_line[i] = 0;
+			argc++;
+		}
+	}
+
+	// The number of args should be one more than the spaces.
+	argc++;
+
+	// Allocate the pointers.
+
+	argv = (char **)calloc(argc, sizeof(char *));
+
+	// Process cmd_line again setting up argv.
+
+	p1 = cmd_line;
+
+	for (i = 0; i < argc; i++)
+	{
+		argv[i] = p1;
+		p2 = strchr(p1, 0);
+		if (p2) p1 = p2 + 1;
+	}
+
+	// Remove any quotes
+
+	for (i = 0; i < argc; i++)
+		if (argv[i][0] == '"')
+			for (l = 0; argv[i][l]; l++)
+			{
+				argv[i][l] = argv[i][l + 1];
+				if (argv[i][l] == '"')
+					argv[i][l] = 0;
+			}
+
+	// Call real program entry point
+
+	ret = go_at_it(argc, argv);
+	fclose(o);
+	return ret;
+}
+
+
+
+/* This is the real main() */
+int go_at_it(int argc, char** argv)
+{
+	char *p, *r, *s, *t;
+	char argname[MAXPATH];
+	int ret, i, extract, genflag, join, cor;
+	long blocksize;
+
+	extract = genflag = join = cor = twolinesend = 0;
+	p = r = s = t = endstr = sendstr = NULLCP;
+	*genpath = *argname = *altname = EOS;
+
+	i = 0;
+	o = stdout;
+
+	/* initialize range array */
+	get_range("1-");
+
+	/* Default blocksize (abt 10000 bytes) */
+	blocksize = 138 * 62;
+
+	while (++i < argc)
+	{
+		if (*argv[i] != '-')
+		{
+			if (!p)
+			{
+				p = argv[i]; /* Name of file to de/encode/correct */
+				continue;
+			}
+			if (!r)
+			{
+				r = argv[i]; /* Searchpath for non-coded file. Needed for */
+				continue; /* generating correction file */
+			}
+		}
+
+		if (!_stricmp(argv[i], "-S")) /* Split option */
+		{
+			i++;
+			if (i == argc)
+			{
+				blocksize = 512 * 62; /* No parameter, set max blocksize */
+				i--;
+			}
+			else if (sscanf(argv[i], "%li", &blocksize) == 1 && *argv[i] != '-')
+				blocksize *= 62L; /* Set blocksize to parameter */
+			else
+				blocksize = 512 * 62; /* Next arg is not a parm. Set max blocksize */
+		}
+
+		if (!_stricmp(argv[i], "-SP")) /* Split into equal parts */
+		{
+			i++;
+			if (i == argc)
+			{
+				blocksize = 0; /* No parameter, no user defined split */
+				i--;
+			}
+			else if (sscanf(argv[i], "%li", &blocksize) == 1)
+				blocksize += 50000L; /* Number of parts to encode (50000 used as
+											 indicator) */
+		}
+
+		if (!_stricmp(argv[i], "-SB")) /* Split in parts of n bytes */
+		{
+			i++;
+			if (i == argc)
+				i--;
+			else if (sscanf(argv[i], "%li", &blocksize) == 1)
+				blocksize = (blocksize / 71 - 2) * 62;
+		}
+
+		if (!_stricmp(argv[i], "-R")) /* Only re-encode specified part(s) */
+		{
+			i++;
+			if (i == argc)
+				i--;
+			else
+				get_range(argv[i]);
+		}
+
+		if (!_stricmp(argv[i], "-TB")) /* File to get head and foot lines from */
+		{
+			i++;
+			if (i == argc)
+			{
+				t = def_format;
+				i--;
+			}
+			else if (*argv[i] != '-')
+				t = argv[i];
+			else
+				t = def_format;
+		}
+
+		if (!_stricmp(argv[i], "-T")) /* Define BBS's termination string, */
+		{ /* e.g. "/ex" */
+			i++;
+			if (i == argc)
+				i--;
+			else
+			{
+				if (t != def_format)
+				{
+					endstr = (char *)malloc((int)strlen(argv[i]) + 1);
+					strcpy(endstr, argv[i]);
+				}
+			}
+		}
+		// Save KRR
+		if (!_strnicmp(argv[i], "-SAVE", 5))
+		{
+			i++;
+			if (i == argc)
+				i--;
+			else
+			{
+				if (t != def_format)
+				{
+					pathstr = (char *)malloc((int)strlen(argv[i]) + 1);
+					strcpy(pathstr, argv[i]);
+				}
+			}
+
+		}
+		//end save
+		if (!_strnicmp(argv[i], "-SEND", 5)) /* Define send string, */
+		{ /* e.g. "sp dg1bbq @db0ver.#nds.deu.eu" */
+			if (argv[i][5] == '2')
+				twolinesend = 1;
+			i++;
+			if (i == argc)
+				i--;
+			else
+			{
+				if (t != def_format)
+				{
+					sendstr = (char *)malloc((int)strlen(argv[i]) + 1);
+					strcpy(sendstr, argv[i]);
+				}
+			}
+		}
+
+		if (!_stricmp(argv[i], "-U")) /* Set alternative filename */
+		{
+			i++;
+			if (i == argc)
+				i--;
+			else
+				strcpy(altname, argv[i]);
+		}
+
+		if (!_stricmp(argv[i], "-#")) /* Create 7PLUS.FLS. Contents e.g.:     */
+			fls = 1; /* 10 TEST */
+		/* for TEST.EXE encoded into 10 parts   */
+
+		if (!_stricmp(argv[i], "-C")) /* Use 7PLUS-file as a correction file  */
+			cor = 1;
+
+		if (!_stricmp(argv[i], "-K")) /* Kill obsolete files, stop if gap */
+			autokill = 1; /* greater than 10 files (faster)   */
+
+		if (!_stricmp(argv[i], "-KA"))/* Kill all obsolete files        */
+			autokill = 2; /* (slow, but better for servers) */
+
+		if (!_stricmp(argv[i], "-F")) /* Force usage of correction file */
+			force = 1;
+
+		if (!_stricmp(argv[i], "-G")) /* Write to same dir as input file */
+			genflag = 1;
+
+		if (!_stricmp(argv[i], "-J")) /* Join two error reports / Produce single */
+			join = 1; /* output file when encoding               */
+
+
+		if (!_stricmp(argv[i], "-P")) /* Write encoded files in Packet format */
+			sprintf(delimit, "\r"); /* for direct binary upload. */
+
+
+
+		if (!_stricmp(argv[i], "-SIM")) /* Simulate encoding and report */
+			simulate = 1; /* number of parts and parts */
+		/* filename in 7plus.fls */
+
+		if (!_stricmp(argv[i], "-SYSOP")) /* SYSOP mode. Decode, even if parts */
+			sysop = 1; /* are missing. */
+
+		if (!_stricmp(argv[i], "-X")) /* Extract 7plus-files from log-file    */
+			extract = 1;
+
+		if (!_stricmp(argv[i], "-Y")) /* Always assume YES on queries.*/
+			noquery = 1;
+	}
+
+	if (!_isatty(_fileno(o)))
+		no_tty = noquery = 1;
+
+	if (no_tty)
+		fprintf(o, "%s", s_logon);
+	else if (!p) /* No File specified, show help */
+	{
+		int scrlines;
+		int n = 5;
+
+		i = 0;
+
+		/* How many lines fit on screen? */
+		scrlines = 40;
+
+
+		ret = 0;
+		if (o != stdout)
+			fclose(o);
+		free(idxptr);
+		return (ret);
+	}
+
+	if ((s = (char *)malloc((size_t)4000UL)) == NULLCP)
+	{
+		fprintf(o, nomem);
+		if (o != stdout)
+			fclose(o);
+
+		exit(21);
+
+	}
+	free(s);
+
+	if ((idxptr = (struct m_index *)malloc(sizeof(struct m_index))) == NULL)
+	{
+		fprintf(o, nomem);
+		if (o != stdout)
+			fclose(o);
+
+		exit(21);
+
+	}
+
+	buflen = 16384;
+
+	init_crctab(); /* Initialize table for CRC-calculation */
+	init_decodetab(); /* decoding-table */
+	init_codetab(); /* encoding-table */
+
+	strcpy(argname, p);
+	// KRR
+	if (pathstr == NULL)
+	{
+		fnsplit(argname, _drive, _dir, _file, _ext);
+	}
+	else
+	{
+		fnsplit(argname, _drive, _dir, _file, _ext);
+
+		char *c = malloc(strlen(pathstr) + strlen(_file) + strlen(_ext) + 1);
+		if (c != NULL)
+		{
+			strcpy(c, pathstr);
+			strcat(c, _file);
+			strcat(c, _ext);
+			strcpy(argname, c);
+			strcpy(genpath, pathstr);
+
+			free(c);
+		}
+
+	}
+	//KRR 
+	// fnsplit(argname, _drive, _dir, _file, _ext);
+	if (genflag)
+		sprintf(genpath, "%s%s", _drive, _dir);
+
+	if (extract)
+	{
+		if (p)
+			ret = extract_files(argname, r);
+		else
+		{
+			fprintf(o, "\007File to extract from not specified. Break.\n");
+			ret = 6;
+		}
+		if (o != stdout)
+			fclose(o);
+		free(idxptr);
+		return (ret);
+	}
+	/* Does the filename contain an extension? */
+	if (*_ext)
+	{
+		if (cor)
+		{
+			ret = correct_meta(argname, 0, 0);
+			if (o != stdout)
+				fclose(o);
+			free(idxptr);
+			return (ret);
+		}
+
+		if (join)
+			if (!_strnicmp(".err", _ext, 4) ||
+				(toupper(*(_ext + 1)) == 'E' &&
+				isxdigit(*(_ext + 2)) &&
+				isxdigit(*(_ext + 3))))
+			{
+				ret = join_control(argname, r);
+				if (o != stdout)
+					fclose(o);
+				free(idxptr);
+				return (ret);
+			}
+
+		if (!_strnicmp(".cor", _ext, 4) ||
+			(toupper(*(_ext + 1)) == 'C' &&
+			isxdigit(*(_ext + 2)) &&
+			isxdigit(*(_ext + 3))))
+		{
+			ret = correct_meta(argname, 1, 0);
+			if (o != stdout)
+				fclose(o);
+			free(idxptr);
+			return (ret);
+		}
+
+		if (sysop)
+		{
+			ret = control_decode(argname);
+			if (o != stdout)
+				fclose(o);
+			free(idxptr);
+			return (ret);
+		}
+
+		/* Call decode_file() if ext ist 7PL, P01, else encode_file() */
+		if (!_strnicmp(".7pl", _ext, 4) || !_strnicmp(".p01", _ext, 4))
+		{
+			ret = control_decode(argname);
+			if (o != stdout)
+				fclose(o);
+			free(idxptr);
+			return (ret);
+		}
+#ifdef _HAVE_CHSIZE
+		if (!_strnicmp(".7mf", _ext, 4))
+#else
+		if (!_strnicmp(".7ix", _ext, 4))
+#endif
+		{
+			ret = make_new_err(argname);
+			if (o != stdout)
+				fclose(o);
+			free(idxptr);
+			return (ret);
+		}
+
+		if (!_strnicmp(".x", _ext, 3))
+		{
+			ret = extract_files(argname, r);
+			if (o != stdout)
+				fclose(o);
+			free(idxptr);
+			return (ret);
+		}
+		ret = encode_file(argname, blocksize, r, join, t, genpath);
+	}
+	else
+	{
+		if (!test_exist(argname)) /* no EXT, but file exists on disk, then encode */
+			ret = encode_file(argname, blocksize, r, join, t, genpath);
+		else
+			ret = control_decode(argname);
+	}
+
+	if (o != stdout)
+		fclose(o);
+	free(idxptr);
+	return (ret);
+}
+
+
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+ *+ Possible return codes:                                                 +*
+ *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
+
+ 0 No errors detected.
+ 1 Write error.
+ 2 File not found.
+ 3 7PLUS header not found.
+ 4 File does not contain expected part.
+ 5 7PLUS header corrrupted.
+ 6 No filename for extracting defined.
+ 7 invalid error report / correction / index file.
+ 8 Max number of parts exceeded.
+ 9 Bit 8 stripped.
+ 10 User break in test_file();
+ 11 Error report generated.
+ 12 Only one or no error report to join
+ 13 Error report/cor-file does not refer to the same original file
+ 14 Couldn't write 7plus.fls
+ 15 Filesize of original file and the size reported in err/cor-file not equal
+ 16 Correction not successful.
+ 17 No CRC found in err/cor-file.
+ 18 Timestamp in metafile differs from that in the correction file.
+ 19 Metafile already exists.
+ 20 Can't encode files with 0 filelength.
+ 21 Not enough memory available
+
+ *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
---
+++ 7plus/7plus.h
@@ -0,0 +1,192 @@
+/*---------*\
+>  7plus.h  <
+\*---------*/
+
+#define _CRT_SECURE_NO_WARNINGS 
+#define YES "yes"
+#define ALWAYS "always"
+#define NO  "no"
+#define EOS '\0'
+#define ON 0
+#define OFF 1
+#define LSEP 0x0a
+#define LSEPS "\x0a"
+#define _7PLUS_FLS "7plus.fls"
+
+
+#include <conio.h>
+
+/* Some compilers are very strict abt the type of NULL-pointers */
+#define NULLFP ((FILE *) 0)
+#define NULLCP ((char *) 0)
+
+
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#include <windows.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <io.h>
+#include <conio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#define MAXDRIVE _MAX_DRIVE
+#define MAXDIR   _MAX_DIR
+#define MAXFILE  _MAX_FNAME
+#define MAXEXT   _MAX_EXT
+#define MAXPATH  _MAX_PATH
+#define fnsplit  _splitpath
+#include <direct.h>
+#define GetCurrentDir _getcwd
+
+#define PATHSEP "\\"
+#define PATHCHAR '\\'
+#define LFN  /* Allow long filenames */
+#define TWO_CHAR_SEP
+#define MAXFNAME MAXPATH
+#define _HAVE_FNSPLIT
+#define _HAVE_CHSIZE
+#define _HAVE_ICMP
+#define _HAVE_GMTIME
+#define _HAVE_MKTIME
+//#define _HAVE_GETCH
+
+#define MAXFPATH (MAXDRIVE+MAXDIR-1)
+
+/* flags for fopen() */
+#define OPEN_READ_TEXT "r"
+#define OPEN_WRITE_TEXT "w"
+#define OPEN_APPEND_TEXT "a"
+#define OPEN_RANDOM_TEXT "r+"
+#ifdef TWO_CHAR_SEP
+/* This is for systems that convert LF to CR/LF in textmode */
+#define OPEN_READ_BINARY "rb"
+#define OPEN_WRITE_BINARY "wb"
+#define OPEN_APPEND_BINARY "ab"
+#define OPEN_RANDOM_BINARY "r+b"
+#else
+/* And this, if there is no conversion */
+#define OPEN_READ_BINARY "r"
+#define OPEN_WRITE_BINARY "w"
+#define OPEN_APPEND_BINARY "a"
+#define OPEN_RANDOM_BINARY "r+"
+#endif
+
+/** shorthands for unsigned types **/
+typedef unsigned char byte; /* 8bit unsigned char */
+
+typedef unsigned int uint; /* 16 or 32bit unsigned int */
+typedef unsigned long ulong; /* 32bit unsigned long      */
+
+
+struct m_index
+{
+	char filename[14]; /*12  chars +2*/
+	char full_name[258];/*256 chars +2*/
+
+	long length;
+	ulong timestamp;
+	int splitsize;
+	ulong lines_ok[4090];
+	long lines_left;
+};
+
+/*********************** macros *************************/
+
+#define crc_calc(x,y) (x)=crctab[(x)>>8]^((((x)&255)<<8)|(byte)(y))
+
+/***************** function prototypes ******************/
+
+/** 7plus.c **/
+int go_at_it(int argc, char** argv);
+int screenlength(void);
+
+/** encode.c **/
+int encode_file(char* name, long blocksize, char* searchbin, int join, char* head_foot, char* genpath);
+void get_range(char* rangestring);
+int read_tb(char* name, char* go_top, char* go_bottom);
+int top_bottom(FILE* wfile, char* top_bot, char* orgname, char* type, int part, int parts);
+/** decode.c **/
+int control_decode(char* name);
+int decode_file(char* name, int flag);
+void decode_n_write(FILE* raus, char* p, int length);
+void w_index_err(struct m_index* idxptr, const char* localname, int flag);
+int make_new_err(const char* name);
+void progress(const char* filename, int part, int of_parts,
+	long errors, long rebuilt, const char* status);
+/* correct.c */
+int correct_meta(char* name, int itsacor, int quietmode);
+
+/** util.c **/
+char* my_fgets(char* string, register int n, FILE* rein);
+int my_putc(int outchar, FILE* out);
+void crc_n_lnum(uint* crc, int* linenumber, char* line);
+void crc2(uint* crc, char* line);
+void add_crc2(char* line);
+int mcrc(char* line, int flag);
+int read_index(FILE* ifile, struct m_index* idxptr);
+int write_index(FILE* ifile, struct m_index* idxptr, int flag);
+ulong read_ulong(FILE* in);
+uint read_uint(FILE* in);
+void write_ulong(FILE* out, ulong val);
+void write_uint(FILE* out, uint val);
+int crc_file(const char* file, const char* s1, const char* s2,
+	int flag);
+int copy_file(const char* to, const char* from, ulong timestamp);
+void replace(const char* oldfil, const char* newfil, ulong timestamp);
+
+
+void kill_em(const char* name, const char* inpath, const char* one,
+	const char* two, const char* three, const char* four,
+	const char* five, int _one, int no_lf);
+void kill_dest(FILE* in, FILE* out, const char* name);
+int test_exist(const char* filename);
+int test_file(FILE* in, char* destnam, int flag, int namsize);
+void init_decodetab(void);
+void init_codetab(void);
+void init_crctab(void);
+void build_DOS_name(char* name, char* ext);
+void strip(char* string);
+
+#ifndef _HAVE_GMTIME
+struct tm *__offtime(const time_t *t, long int offset);
+struct tm *gmtime(const time_t *t);
+
+#ifndef _HAVE_MKTIME
+time_t    mktime(register struct tm *tp);
+#endif
+ulong get_filetime(const char *filename);
+void  set_filetime(const char *filename, ulong ftimestamp);
+#endif
+uint get_hex(char* hex);
+#ifndef _HAVE_FNSPLIT
+void  fnsplit(char *pth, char *dr, char *pa, char *fn, char *ft);
+#endif
+#ifndef _HAVE_ICMP
+char  *_strupr(char *string);
+char  *_strlwr(char *string);
+char  *strcnvt(char *string, int flag);
+int   _stricmp(const char *s1, const char *s2);
+int   _strnicmp(const char *s1, const char *s2, size_t n);
+#endif
+
+/** rebuild.c **/
+int rebuild(char* line, int flag);
+
+/** extract.c **/
+int extract_files(char* name, char* search);
+
+/** join.c **/
+int join_control(char* file1, char* file2);
+int join_err(char* file1, char* file2);
+
+#define set_autolf(x)
+#define check_fn(x)
