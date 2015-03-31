74e4ebc35505f4cc356aa87a7c48da874c308e7b
Kevin <kevin@na7kr.us>
2015-03-31 16:35:21 -0700

    date

--- TerminalControl/Properties/AssemblyInfo.cs
+++ TerminalControl/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NA7KR")]
 [assembly: AssemblyProduct("PacketComs.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
