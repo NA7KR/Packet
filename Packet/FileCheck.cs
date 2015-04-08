6e5c3154391ccdb5652c5123531f8d1257484e74
Kevin <kevin@na7kr.us>
2015-04-08 15:30:27 -0700

    7lpus work

--- Packet/FileCheck.cs
+++ Packet/FileCheck.cs
@@ -5,28 +5,21 @@ using System.Windows.Forms;
 
 namespace Packet
 {
-    class FileCheck
+    internal class FileCheck
     {
         [DllImport("7plus.dll")]
         public static extern int Do_7plus([MarshalAs(UnmanagedType.LPStr)] string args);
 
-        private void button_ok_Click(object sender, EventArgs e)
-        {
-            var fbd = new OpenFileDialog();
-            if (fbd.ShowDialog() == DialogResult.OK)
-
-            {
-                var fp = (Path.GetFullPath(fbd.FileName));
-                var args = fp + " -SAVE \"c:\\temp\\out\\\"";
-                Do_7plus(args);
-            }
 
 
-            //    c:\temp\7plus.zip -SAVE "c:\temp\"  -SB 5000          
-            //    c:\temp\7plus.p01 - SAVE "c:\temp\"                 
-            //    c:\temp\7plus.err -SAVE "c:\temp\"					
-            //	  c:\temp\7plus.cor -SAVE "c:\temp\	
-        }
+
+
+
+        //    c:\temp\7plus.zip -SAVE "c:\temp\"  -SB 5000          
+        //    c:\temp\7plus.p01 - SAVE "c:\temp\"                 
+        //    c:\temp\7plus.err -SAVE "c:\temp\"					
+        //	  c:\temp\7plus.cor -SAVE "c:\temp\	
+
 
         public void CreateFileWatcher(string path)
         {
@@ -34,17 +27,19 @@ namespace Packet
             FileSystemWatcher watcher = new FileSystemWatcher
             {
                 Path = path,
-                NotifyFilter =  NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                NotifyFilter =
+                    NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName |
+                    NotifyFilters.DirectoryName,
                 Filter = "*"
             };
             /* Watch for changes in LastAccess and LastWrite times, and   the renaming of files or directories. */
-            
+
 
             // Add event handlers.
             watcher.Changed += OnChanged;
             watcher.Created += OnChanged;
-            watcher.Deleted += OnChanged;
             watcher.Renamed += OnRenamed;
+            watcher.Renamed += OnChanged;
 
             // Begin watching.
             watcher.EnableRaisingEvents = true;
@@ -54,13 +49,22 @@ namespace Packet
         private static void OnChanged(object source, FileSystemEventArgs e)
         {
             // Specify what is done when a file is changed, created, or deleted.
+            
+            string ext = Path.GetExtension(e.FullPath);
+            string file = Path.GetFileNameWithoutExtension(e.FullPath);
+            string path = Path.GetDirectoryName(e.FullPath) + Path.DirectorySeparatorChar;
+
+            string newfile = path + file + ".P01";
+            var args = newfile + " -SAVE \"c:\\temp\\out\\\"";
+            Do_7plus(args);
             MessageBox.Show("File: " + e.FullPath + " " + e.ChangeType);
         }
 
         private static void OnRenamed(object source, RenamedEventArgs e)
         {
             // Specify what is done when a file is renamed.
-            MessageBox.Show("File: {0} renamed to {1}" +  e.OldFullPath + e.FullPath);
+            MessageBox.Show("File: {0} renamed to {1}" + e.OldFullPath + e.FullPath);
+
         }
     }
 }
