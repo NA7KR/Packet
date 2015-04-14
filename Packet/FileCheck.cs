3aa2b8e6979ee43cbf87f7ab5fa74dc5d359a789
Kevin <kevin@na7kr.us>
2015-04-14 10:41:50 -0700

    file lock

--- Packet/FileCheck.cs
+++ Packet/FileCheck.cs
@@ -21,21 +21,20 @@ namespace Packet
         public void CreateFileWatcher(string path)
         {
             // Create a new FileSystemWatcher and set its properties.
-            FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = path;
-            watcher.NotifyFilter = NotifyFilters.LastWrite;
-            watcher.Filter = "*.*";
+            FileSystemWatcher watcher = new FileSystemWatcher
+            {
+                Path = path,
+                NotifyFilter = NotifyFilters.LastWrite,
+                Filter = "*.*"
+            };
             watcher.Changed += OnChanged;
             watcher.EnableRaisingEvents = true;
 
         }
 
-        // Define the event handlers.
+        #region OnChange
         private static void OnChanged(object source, FileSystemEventArgs e)
         {
-
-
-
             // Specify what is done when a file is changed, created, or deleted.
             string newfile;
             string ext = Path.GetExtension(e.FullPath);
@@ -44,50 +43,181 @@ namespace Packet
             if (ext == ".7pl")
             {
                 newfile = path + file + ".7pl";
+                string lockfile = newfile + ".lock";
+                if (!File.Exists(lockfile))
+                {
+                    using (File.Create(lockfile))
+                    {
+                        var args = newfile + " -SAVE \"c:\\temp\\out\\\"";
+                        Run7Plus(args);
+                    }
+                }
+                else
+                {
+                    File.Delete(lockfile);
+                }
             }
             else
             {
                 newfile = path + file + ".P01";
+                string lockfile = newfile + ".lock";
+                if (!File.Exists(lockfile))
+                {
+                    using (File.Create(lockfile))
+                    {
+                        var args = newfile + " -SAVE \"c:\\temp\\out\\\"";
+                        Run7Plus(args);
+                    }
+                }
+                else
+                {
+                    File.Delete(lockfile);
+                }
             }
 
-            var args = newfile + " -SAVE \"c:\\temp\\out\\\"";
+        }
+        #endregion OnChange 
 
+        #region
+
+        public static void Run7Plus(string newfile)
+        {
+            var args = newfile + " -SAVE \"c:\\temp\\out\\\"";
             Thread.Sleep(5000);
             int rn = Do_7plus(args);
-            MessageBox.Show(rn.ToString());
-
-
-
-            //MessageBox.Show("File: " + e.FullPath + " " + e.ChangeType);
-            /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
-            *+ Possible return codes:                                                 +*
-            *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*
-
-            0 No errors detected.
-            1 Write error.
-            2 File not found.
-            3 7PLUS header not found.
-            4 File does not contain expected part.
-            5 7PLUS header corrupted.
-            6 No filename for extracting defined.
-            7 invalid error report / correction / index file.
-            8 Max number of parts exceeded.
-            9 Bit 8 stripped.
-            10 User break in test_file();
-            11 Error report generated.
-            12 Only one or no error report to join
-            13 Error report/cor-file does not refer to the same original file
-            14 Couldn't write 7plus.fls
-            15 File size of original file and the size reported in err/cor-file not equal
-            16 Correction not successful.
-            17 No CRC found in err/cor-file.
-            18 Time stamp in meta file differs from that in the correction file.
-            19 Meta file already exists.
-            20 Can't encode files with 0 file length.
-            21 Not enough memory available
-
-            *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+            Msg(newfile, rn);
         }
+        #endregion
+
+        #region
+        public static void Msg(string newfile, int rn)
+        {
+            string txt;
+            switch (rn)
+            {
+                case 0:
+                    {
+                        txt = "No errors detected.";
+                        break;
+                    }
+                case 1:
+                    {
+                        txt = "Write error.";
+                        break;
+                    }
+                case 2:
+                    {
+                        txt = "File not found.";
+                        break;
+                    }
+                case 3:
+                    {
+                        txt = "7PLUS header not found.";
+                        break;
+                    }
+                case 4:
+                    {
+                        txt = "File does not contain expected part.";
+                        break;
+                    }
+                case 5:
+                    {
+                        txt = "7PLUS header corrupted.";
+                        break;
+                    }
+                case 6:
+                    {
+                        txt = "No filename for extracting defined.";
+                        break;
+                    }
+                case 7:
+                    {
+                        txt = "invalid error report / correction / index file.";
+                        break;
+                    }
+                case 8:
+                    {
+                        txt = "Max number of parts exceeded.";
+                        break;
+                    }
+                case 9:
+                    {
+                        txt = "Bit 8 stripped.";
+                        break;
+                    }
+                case 10:
+                    {
+                        txt = "User break in test_file();";
+                        break;
+                    }
+                case 11:
+                    {
+                        txt = "Error report generated.";
+                        break;
+                    }
+                case 12:
+                    {
+                        txt = "Only one or no error report to join.";
+                        break;
+                    }
+                case 13:
+                    {
+                        txt = "Error report/cor-file does not refer to the same original file.";
+                        break;
+                    }
+                case 14:
+                    {
+                        txt = "Couldn't write 7plus.fls.";
+                        break;
+                    }
+                case 15:
+                    {
+                        txt = "File size of original file and the size reported in err/cor-file not equal.";
+                        break;
+                    }
+                case 16:
+                    {
+                        txt = "Correction not successful.";
+                        break;
+                    }
+                case 17:
+                    {
+                        txt = "No CRC found in err/cor-file.";
+                        break;
+                    }
+                case 18:
+                    {
+                        txt = "Time stamp in meta file differs from that in the correction file.";
+                        break;
+                    }
+
+                case 19:
+                    {
+                        txt = "Meta file already exists.";
+                        break;
+                    }
+                case 20:
+                    {
+                        txt = "Can't encode files with 0 file length.";
+                        break;
+                    }
+                case 21:
+                    {
+                        txt = " Not enough memory available.";
+                        break;
+                    }
+
+
+                default:
+                    {
+                        txt = "?";
+                        break;
+                    }
+
+            }
+            MessageBox.Show(txt + " " + newfile);
+        }
+        #endregion
 
     }
 }
